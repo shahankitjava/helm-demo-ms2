ae1fce340a30aea21bedcaf2e16a23266b752b5c
Ankit Shah <ankit.shah@aa.com>
2020-04-06 05:58:42 -0500

    Added spring boot project

---
+++ src/main/java/com/aa/helm/helmdemoms2/HelloController.java
@@ -0,0 +1,18 @@
+package com.aa.helm.helmdemoms2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+@GetMapping(value="/helm2")
+public @ResponseBody HelmResponse greet(@PathVariable String id) {
+    HelmResponse helmResponse = new HelmResponse();
+    helmResponse.setWelcome("Helm 2 - Demo MS 2 : " + id);
+    return helmResponse;
+}
+
+}
---
+++ src/main/java/com/aa/helm/helmdemoms2/HelmResponse.java
@@ -0,0 +1,13 @@
+package com.aa.helm.helmdemoms2;
+
+public class HelmResponse {
+    private String welcome;
+
+    public String getWelcome() {
+        return welcome;
+    }
+
+    public void setWelcome(String welcome) {
+        this.welcome = welcome;
+    }
+}
