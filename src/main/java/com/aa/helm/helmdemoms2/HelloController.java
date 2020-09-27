5b90d09f6a54e930c68e7dcd6b161116118eff47
Ankit Shah <ankit.shah@aa.com>
2020-09-27 01:50:47 -0500

    Changed app

--- src/main/java/com/aa/helm/helmdemoms2/HelloController.java
+++ src/main/java/com/aa/helm/helmdemoms2/HelloController.java
@@ -15,17 +15,28 @@ public class HelloController {
     @Value("${helm3.url}")
     private URI helm3Url;
 
-    @GetMapping(value = "/")
+    @GetMapping(value = "/helm2")
     public @ResponseBody
     HelmResponse greet() {
+        System.out.println("===== Method greet called of helm-demo-ms2 =====");
         HelmResponse helmResponse = new HelmResponse();
         helmResponse.setWelcome("Hello from helm-demo-ms2");
         return helmResponse;
     }
 
-    @GetMapping(value = "/callHelm3")
+    @GetMapping(value = "/helm2/callFromMS1")
+    public @ResponseBody
+    HelmResponse greetCallFromMS1() {
+        System.out.println("===== Method greetCallFromMS1 called of helm-demo-ms2 by helm-demo-ms1 =====");
+        HelmResponse helmResponse = new HelmResponse();
+        helmResponse.setWelcome("Call came from MS1 and responded from MS2");
+        return helmResponse;
+    }
+
+    @GetMapping(value = "/helm2/callHelm3")
     public @ResponseBody
     HelmResponse greetHelm3() {
+        System.out.println("===== Method greetHelm3 called of helm-demo-ms2 =====");
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<HelmResponse> entity = restTemplate.getForEntity(helm3Url, HelmResponse.class);
         return entity.getBody();
